ea71a9b05973486a339bbd33d3e5696aaaed4792
Shashikant Jaiswal <shashikant.jaiswal@broadcom.com>
2018-07-25 22:01:57 +0530

    Calling another microservice from one microservice using Eureka-service discovery

--- Microservice-A/src/main/java/com/broadcom/ms/MicroserviceA/resource/AResource.java
+++ Microservice-A/src/main/java/com/broadcom/ms/MicroserviceA/resource/AResource.java
@@ -1,15 +1,30 @@
 package com.broadcom.ms.MicroserviceA.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/msa")
 public class AResource {
 	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping("/")
 	public String getName() {
-		return "Hello service A";
+		
+		String response = "Hello service A and ";
+		
+		//Calling another service registered with Eureka server
+		ResponseEntity<String> responseEntity = 
+		restTemplate.exchange("http://microservice-b/msb/", HttpMethod.GET, null, String.class);
+		response = response + responseEntity.getBody();
+		
+		return response;
 	}
 }
